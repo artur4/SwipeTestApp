5322defa38763e42e1d415ab56b44fb4d6cf9547
Artur Mirrov <arturmirrov@gmail.com>
2016-07-04 14:49:00 +0300

    Initial Commit

---
+++ SwipeTestApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwipeTestApp
+//
+//  Created by Artur Mirrov on 04.07.16.
+//  Copyright © 2016 Artur Mirrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SwipeTestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SwipeTestApp
+//
+//  Created by Artur Mirrov on 04.07.16.
+//  Copyright © 2016 Artur Mirrov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
